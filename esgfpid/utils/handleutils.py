b64558a86ebf6914dd383b2cf161fbcca67cbfc1
merret <buurman@dkrz.de>
2016-10-18 14:01:07 +0200

    Splitted hash-making util function into several (for reuse).

--- esgfpid/utils/handleutils.py
+++ esgfpid/utils/handleutils.py
@@ -4,11 +4,17 @@ from .argsutils import check_presence_of_mandatory_args
 def make_handle_from_drsid_and_versionnumber(**args):
     check_presence_of_mandatory_args(args, ['drs_id','version_number','prefix'])
     suffix = make_suffix_from_drsid_and_versionnumber(drs_id=args['drs_id'], version_number=args['version_number'])
-    return 'hdl:'+args['prefix']+'/'+suffix
+    return _suffix_to_handle(args['prefix'], suffix)
+
+def _suffix_to_handle(prefix, suffix):
+    return 'hdl:'+prefix+'/'+suffix
 
 def make_suffix_from_drsid_and_versionnumber(**args):
     check_presence_of_mandatory_args(args, ['drs_id','version_number'])
     hash_basis = args['drs_id']+'.v'+str(args['version_number'])
+    return _make_uuid_from_basis(hash_basis)
+
+def _make_uuid_from_basis(hash_basis):
     hash_basis_utf8 = hash_basis.encode('utf-8')
     ds_uuid = uuid.uuid3(uuid.NAMESPACE_URL, hash_basis_utf8) # Using uuid3, as this is easy to use also with Java
     return str(ds_uuid)
