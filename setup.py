36f7d60b209a06217f6d71daf3d2230694dba9de
merret <buurman@dkrz.de>
2016-12-21 12:30:57 +0100

    Version to 0.5.1, add download URL to push to pypi.

--- setup.py
+++ setup.py
@@ -44,10 +44,11 @@ except(IOError, ImportError):
 
 setup(
     name='esgfpid',
-    version='0.5',
+    version='0.5.1',
     author='Merret Buurman, German Climate Computing Centre (DKRZ)',
     author_email='buurman@dkrz.de',
     url='https://github.com/IS-ENES-Data/esgf-pid',
+    download_url='https://github.com/IS-ENES-Data/esgf-pid/archive/0.5.1.tar.gz',
     description='Library for sending PID requests to a rabbit messaging queue during ESGF publication.',
     long_description=long_description,
     packages=packages + test_packages,
