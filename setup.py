f0cb6fca6307f602c26ed48af65b70c4fa4450e3
merret <buurman@dkrz.de>
2017-02-16 19:07:43 +0100

    Finish version 0.7.0.

--- setup.py
+++ setup.py
@@ -44,11 +44,11 @@ except(IOError, ImportError):
 
 setup(
     name='esgfpid',
-    version='0.6.4-dev',
+    version='0.7.0',
     author='Merret Buurman, German Climate Computing Centre (DKRZ)',
     author_email='buurman@dkrz.de',
     url='https://github.com/IS-ENES-Data/esgf-pid',
-    download_url='https://github.com/IS-ENES-Data/esgf-pid/archive/0.6.4-dev.tar.gz',
+    download_url='https://github.com/IS-ENES-Data/esgf-pid/archive/0.7.0.tar.gz',
     description='Library for sending PID requests to a rabbit messaging queue during ESGF publication.',
     long_description=long_description,
     packages=packages + test_packages,
