125352eef2b92ae3ced725e3e9288bc2fcb165f8
merret <buurman@dkrz.de>
2017-02-17 18:17:02 +0100

    Added simple state-dependent behaviour to rabbit.py.

--- esgfpid/rabbit/rabbit.py
+++ esgfpid/rabbit/rabbit.py
@@ -3,6 +3,7 @@ import esgfpid.utils
 from esgfpid.rabbit.asynchronous import AsynchronousRabbitConnector
 import esgfpid.rabbit.nodemanager
 from esgfpid.utils import logwarn
+from .asynchronous.exceptions import OperationNotAllowed
 
 # Normal logger:
 LOGGER = logging.getLogger(__name__)
@@ -37,7 +38,7 @@ class RabbitMessageSender(object):
         self.__node_manager = self.__make_rabbit_settings(args)
 
         '''
-        TODO
+        Adds a test flag to all test publications.
         '''
         self.__test_publication = args['test_publication']
 
@@ -45,7 +46,12 @@ class RabbitMessageSender(object):
         This creates the object that does
         the actual communications.
         '''
-        self.__server_connector = AsynchronousRabbitConnector(__node_manager)
+        self.__server_connector = AsynchronousRabbitConnector(self.__node_manager)
+
+        self.__state = self.__state_not_started
+        self.NOT_STARTED = 0
+        self.CALLED_START = 1
+        self.CALLED_FINISH = 2
 
 
     def __make_rabbit_settings(self, args):
@@ -86,15 +92,60 @@ class RabbitMessageSender(object):
     ###########################################
 
     def start(self):
+
+        if self.__state == self.NOT_STARTED:
+            self.__start()
+
+        elif self.__state == self.CALLED_START:
+            LOGGER.debug('Calling start again has no effect.')
+
+        elif self.__state == self.CALLED_FINISH:
+            LOGGER.debug('Calling start after having called finish has no effect.')
+
+    def __start(self):
+        self.__state = self.CALLED_START
         self.__server_connector.start_rabbit_thread()
 
     def finish(self):
+        if self.__state == self.NOT_STARTED:
+            LOGGER.debug('Calling finish without having started has no effect.')
+
+        elif self.__state == self.CALLED_START:
+            self.__finish()
+
+        elif self.__state == self.CALLED_FINISH:
+            LOGGER.debug('Calling finish again has no effect.')
+
+    def __finish(self):
+        self.__state = self.CALLED_FINISH
         self.__server_connector.finish_rabbit_thread()
 
     def force_finish(self):
+        if self.__state == self.NOT_STARTED:
+            LOGGER.debug('Calling finish without having started has no effect.')
+
+        elif self.__state == self.CALLED_START or self.__state == self.CALLED_FINISH:
+            self.__force_finish()
+
+    def __force_finish(self):
+        self.__state = self.CALLED_FINISH
         self.__server_connector.force_finish_rabbit_thread()
 
     def send_message_to_queue(self, message):
+
+        if self.__state == self.NOT_STARTED:
+            msg = 'Cannot send requests if thread was not started!'
+            raise OperationNotAllowed(msg)
+
+        elif self.__state == self.CALLED_FINISH:
+            msg = 'Cannot send requests if thread was stopped!'
+            raise OperationNotAllowed(msg)
+
+        elif self.__state == self.CALLED_START:
+            self.__send_message_to_queue(message)
+            
+
+    def __send_message_to_queue(self, message):
         if self.__test_publication == True:
             message['test_publication'] = True
         self.__server_connector.send_message_to_queue(message)
