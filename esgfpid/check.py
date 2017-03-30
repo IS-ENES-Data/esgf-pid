024f4a1cf35d987f5761561b099877b86be49dc5
merret <buurman@dkrz.de>
2017-03-30 17:09:48 +0200

    Fixed check.py module.

--- esgfpid/check.py
+++ esgfpid/check.py
@@ -21,13 +21,10 @@ class RabbitChecker(object):
     #
 
     def __init__(self, **args):
-        mandatory_args = ['messaging_service_username', 'messaging_service_password']
-        optional_args = ['messaging_service_url_preferred', 'messaging_service_urls', 'print_to_console', 'messaging_service_exchange_name', 'print_success_to_console']
+        mandatory_args = ['connector']
+        optional_args = ['print_to_console', 'print_success_to_console']
         check_presence_of_mandatory_args(args, mandatory_args)
         add_missing_optional_args_with_value_none(args, optional_args)
-        self.__check_if_any_url_specified(args)
-        self.__rename_url_args(args)
-        self.__adapt_url_args(args)
         self.__define_all_attributes()
         self.__fill_all_attributes(args)
         self.connection = None
@@ -39,41 +36,16 @@ class RabbitChecker(object):
         self.__print_success_to_console = False
         self.__default_log_level = logging.DEBUG
         self.__error_messages = []
-        self.__rabbit_username = None
-        self.__rabbit_password = None
-        self.__rabbit_hosts = None # all passed hosts that are NOT the current one!
-        self.__current_rabbit_host = None # the only host OR the preferred OR a randomly chosen
+        self.__nodemanager = None
+        self.__current_rabbit_host = None
         self.__exchange_name = None
 
-    def __check_if_any_url_specified(self, args):
-        preferred_given = args['messaging_service_url_preferred'] is not None
-        other1 = args['messaging_service_urls'] is not None
-        other2 = len(args['messaging_service_urls']) > 0 if other1 else False
-        other_given = (other1 and other2)
-        if not (preferred_given or other_given):
-            raise esgfpid.exceptions.ArgumentError('At least one messaging service URL has to be specified.') 
-
-    def __rename_url_args(self, args):
-        args['urls_fallback'] = args['messaging_service_urls']
-        del args['messaging_service_urls']
-        args['url_preferred'] = args['messaging_service_url_preferred']
-        del args['messaging_service_url_preferred']
-
-    def __adapt_url_args(self, args):
-        esgfpid.rabbit.rabbitutils.ensure_urls_are_a_list(args, LOGGER)
-        esgfpid.rabbit.rabbitutils.set_preferred_url(args, LOGGER)
-
     def __fill_all_attributes(self, args):
-        self.__rabbit_username = args['messaging_service_username']
-        self.__rabbit_password = args['messaging_service_password']
-        self.__rabbit_hosts = args['urls_fallback']
-        self.__current_rabbit_host = args['url_preferred']
+        self.__nodemanager = args['connector']._Connector__coupler._Coupler__rabbit_message_sender._RabbitMessageSender__node_manager
         if args['print_to_console'] is not None and args['print_to_console'] == True:
             self.__print_errors_to_console = True
         if args['print_success_to_console'] is not None and args['print_success_to_console'] == True:
             self.__print_success_to_console = True
-        if args['messaging_service_exchange_name'] is not None:
-            self.__exchange_name = args['messaging_service_exchange_name']
 
 
     #
@@ -130,9 +102,10 @@ class RabbitChecker(object):
 
             except ValueError as e:
 
-                if self.__is_url_left(): # stay in loop, try next host
-                    utils.logtrace(LOGGER, 'Left URLs: %s', self.__rabbit_hosts)
-                    self.__set_next_url()
+                if self.__nodemanager.has_more_urls(): # stay in loop, try next host
+                    utils.logtrace(LOGGER, 'Left URLs: %s', self.__nodemanager.get_num_left_urls())
+                    self.__nodemanager.set_next_host()
+                    self.__current_rabbit_host = self.__nodemanager.get_connection_parameters().host
                     utils.logtrace(LOGGER, 'Now trying: %s', self.__current_rabbit_host)
 
                 else: # definitive fail, leave loop
@@ -140,20 +113,13 @@ class RabbitChecker(object):
 
         return success
             
-    def __is_url_left(self):
-        if len(self.__rabbit_hosts) > 0:
-            return True
-        return False
-
-    def __set_next_url(self):
-        self.__current_rabbit_host = self.__rabbit_hosts.pop()
-
     #
     # Building connections:
     #
 
     def __check_exchange_existence(self, channel):
 
+        self.__exchange_name = self.__nodemanager.get_exchange_name()
         if self.__exchange_name is not None:
             try:
                 self.__loginfo(' .. checking exchange ...')
@@ -162,7 +128,8 @@ class RabbitChecker(object):
             except (pika.exceptions.ChannelClosed) as e:
                 self.__loginfo(' .. checking exchange ... failed.')
                 self.__add_error_message_no_exchange()
-                raise ValueError('The exchange %s does not exist on messaging service host %s' % (self.__exchange_name, self.__current_rabbit_host))
+                raise ValueError('The exchange %s does not exist on messaging service host %s' %
+                    (self.__exchange_name, self.__current_rabbit_host))
         else:
             pass # No exchange name was given
 
@@ -209,14 +176,8 @@ class RabbitChecker(object):
         return connection
 
     def __open_rabbit_connection(self):
-        credentials = pika.PlainCredentials(
-            self.__rabbit_username,
-            self.__rabbit_password
-        )
-        params = pika.ConnectionParameters( # https://pika.readthedocs.org/en/0.9.6/connecting.html
-            host=self.__current_rabbit_host,
-            credentials=credentials
-        )
+        params = self.__nodemanager.get_connection_parameters()
+        self.__current_rabbit_host = params.host
         connection = self.__pika_blocking_connection(params)
         return connection
 
@@ -244,8 +205,8 @@ class RabbitChecker(object):
     def __add_error_message_authentication_error(self):
         msg = (' - host "%s": Authentication failure (user %s, password %s).' % (
             self.__current_rabbit_host,
-            self.__rabbit_username,
-            self.__rabbit_password
+            self.__nodemanager.get_connection_parameters().credentials.username,
+            self.__nodemanager.get_connection_parameters().credentials.password
         ))
         self.__error_messages.append(msg)
 
