8518221a9731189ee607102641254a6811d652de
merret <buurman@dkrz.de>
2017-04-24 18:26:05 +0200

    Added new pika errors, e.g. ssl protocol error or wrong vhost name.

--- esgfpid/rabbit/asynchronous/thread_builder.py
+++ esgfpid/rabbit/asynchronous/thread_builder.py
@@ -155,11 +155,21 @@ class ConnectionBuilder(object):
                 time_passed = datetime.datetime.now() - self.__start_connect_time
                 time_passed_seconds = time_passed.total_seconds()
 
+                # Some pika errors:
                 if isinstance(e, pika.exceptions.ProbableAuthenticationError):
                     errorname = self.__make_error_name(e, 'e.g. wrong user or password')
                     logerror(LOGGER, 'Could not connect: %s (connection failure after %s seconds)', errorname, time_passed_seconds)
                     self.statemachine.detail_authentication_exception = True # TODO WHAT FOR?
 
+                elif isinstance(e, pika.exceptions.ProbableAccessDeniedError):
+                    errorname = self.__make_error_name(e, 'e.g. wrong virtual host name')
+                    logerror(LOGGER, 'Could not connect: %s (connection failure after %s seconds)', errorname, time_passed_seconds)
+                
+                elif isinstance(e, pika.exceptions.IncompatibleProtocolError):
+                    errorname = self.__make_error_name(e, 'e.g. trying TLS/SSL on wrong port')
+                    logerror(LOGGER, 'Could not connect: %s (connection failure after %s seconds)', errorname, time_passed_seconds)
+
+                # Other errors:
                 else:
                     errorname = self.__make_error_name(e)
                     logerror(LOGGER, 'Unexpected error during event listener\'s lifetime (after %s seconds): %s', time_passed_seconds, errorname)
