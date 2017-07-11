7357d9209966f246b61a05db8a4deb85e4ecc605
merret <buurman@dkrz.de>
2017-07-11 15:11:26 +0200

    Cosmetics: Added docstring.

--- esgfpid/utils/routingkeys.py
+++ esgfpid/utils/routingkeys.py
@@ -1,6 +1,41 @@
+'''
+The ESGF PID system relies on a RabbitMQ instance consisting
+of various exchanges and queues. Messages are routed to
+these queues using routing keys.
 
+This file contains the various routing keys that are used.
+
+The esgfpid library uses routing keys for topic exchanges,
+consisting of four "words" (separated by dots). The first
+word is the prefix (but without dots), the second one is
+the word "HASH" (placeholder for possibe future load balancing
+using hash values), the third is an instruction for the Rabbit,
+and the fourth one is a short description of the type of message
+being sent.
+
+This way, messages can be routed to different consumers based
+on the prefix, they can be prioritized based on their content,
+and filtered using some instructions.
+
+'''
+
+
+
+'''
+This is the Rabbit instruction. It tells the RabbitMQ system
+that the message received is a freshly published one, in
+opposition to messages that have been parked and are retried
+several times.
+Please note that the instruction can be extended by the
+functions below.
+'''
 RABBIT_INSTRUCTION = 'fresh'
 
+
+'''
+These are the templates for the routing keys, based on the
+message's content.
+'''
 ROUTING_KEYS = dict(
     publi_file = 'PREFIX.HASH.'+RABBIT_INSTRUCTION+'.publi-file-orig',
     publi_file_rep = 'PREFIX.HASH.'+RABBIT_INSTRUCTION+'.publi-file-repli',
@@ -13,9 +48,18 @@ ROUTING_KEYS = dict(
     data_cart = 'PREFIX.HASH.'+RABBIT_INSTRUCTION+'.datacart'
 )
 
+
+'''
+These three keywords may be added to the Rabbit instruction
+part of the routing key, in case a message is not sent
+via a trusted RabbitMQ node.
+Currently, this should not happen, as we raise an exception
+if an untrusted node is used.
+'''
 ROUTING_KEY_INTERFIX_UNSURE_IF_TRUSTED = 'untrusted-unsure'
 ROUTING_KEY_INTERFIX_UNTRUSTED_AS_FALLBACK = 'untrusted-fallback'
 ROUTING_KEY_INTERFIX_UNTRUSTED = 'untrusted-only'
+
 
 def adapt_routing_key_for_untrusted(routing_key):
     routing_key = routing_key.replace('fresh', 'fresh-'+ROUTING_KEY_INTERFIX_UNTRUSTED)
@@ -29,6 +73,19 @@ def adapt_routing_key_for_untrusted_unsure(routing_key):
     routing_key = routing_key.replace('fresh', 'fresh-'+ROUTING_KEY_INTERFIX_UNSURE_IF_TRUSTED)
     return routing_key
 
-RABBIT_DEFAULT_ROUTING_KEY='fallback.fallback.'+RABBIT_INSTRUCTION+'.fallback' # Default, if none is included in message
-RABBIT_EMERGENCY_ROUTING_KEY='UNROUTABLE.UNROUTABLE.'+RABBIT_INSTRUCTION+'.UNROUTABLE' # If the message was returned as unroutable by the sender
+'''
+The default routing key, in case no key is included
+in the message. This can only happen if there is some
+severe error in the library. Or in testing situations.
+'''
+RABBIT_DEFAULT_ROUTING_KEY='fallback.fallback.'+RABBIT_INSTRUCTION+'.fallback'
+
+'''
+The default routing key, in case a message comes back
+from the RabbitMQ as "unroutable". This can only happen
+if there is a severe problem with the RabbitMQ federation
+(i.e. the exchanges and bindings are not correct, the
+alternate-exchange did not catch the message).
+'''
+RABBIT_EMERGENCY_ROUTING_KEY='UNROUTABLE.UNROUTABLE.'+RABBIT_INSTRUCTION+'.UNROUTABLE'
 
