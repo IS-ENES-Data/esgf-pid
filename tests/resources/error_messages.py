59f330eb8000005c436c8071fb5bb0ee1236188a
merret <buurman@dkrz.de>
2019-04-02 10:11:41 +0200

    Unit tests for check method: Added the lower priority stuff to expected message. Split expected outcome into stdout part and returned part.

--- tests/resources/error_messages.py
+++ tests/resources/error_messages.py
@@ -1,7 +1,7 @@
 # Expected error messages for the unit tests
 # of the check.py module.
 
-expected_message_ok = '''Checking config for PID module (rabbit messaging queue) ...
+expected_message_ok1 = '''Checking config for PID module (rabbit messaging queue) ...
  .. checking authentication and connection ...
  .. checking authentication and connection (this.is.my.only.host)... ok.
  .. checking authentication and connection ... ok.
@@ -12,11 +12,14 @@ expected_message_ok = '''Checking config for PID module (rabbit messaging queue)
 Config for PID module (rabbit messaging queue).. ok.
 Successful connection to PID messaging queue at "this.is.my.only.host".'''
 
+expected_message_ok2 = None # no message returned when success occurs!
 
-expected_message_first_connection_failed_then_ok = '''Checking config for PID module (rabbit messaging queue) ...
+expected_message_first_connection_failed_then_ok1 = '''Checking config for PID module (rabbit messaging queue) ...
  .. checking authentication and connection ...
  .. checking connection (this.is.my.favourite.host)... FAILED.
+ .. giving this node a lower priority..
  .. checking connection (mystery-tour.uk)... FAILED.
+ .. giving this node a lower priority..
  .. checking authentication and connection (tomato.salad-with-spam.fr)... ok.
  .. checking authentication and connection ... ok.
  .. checking channel ...
@@ -26,12 +29,15 @@ expected_message_first_connection_failed_then_ok = '''Checking config for PID mo
 Config for PID module (rabbit messaging queue).. ok.
 Successful connection to PID messaging queue at "tomato.salad-with-spam.fr".'''
 
+expected_message_first_connection_failed_then_ok2 = None # no message returned when success occurs!
 
-expected_message_connection_failed_unknown = '''Checking config for PID module (rabbit messaging queue) ...
+expected_message_connection_failed_unknown1 = '''Checking config for PID module (rabbit messaging queue) ...
  .. checking authentication and connection ...
  .. checking connection (this.is.my.only.host)... FAILED.
-Config for PID module (rabbit messaging queue) .. FAILED!
-********************************************************************
+ .. giving this node a lower priority..
+Config for PID module (rabbit messaging queue) .. FAILED!'''
+
+expected_message_connection_failed_unknown2 = '''********************************************************************
 *** PROBLEM IN SETTING UP                                        ***
 *** RABBIT MESSAGING QUEUE (PID MODULE)                          ***
 *** CONNECTION TO THE PID MESSAGING QUEUE FAILED DEFINITIVELY:   ***
@@ -40,39 +46,47 @@ Config for PID module (rabbit messaging queue) .. FAILED!
 ********************************************************************'''
 
 
-expected_message_connection_failed_only = '''Checking config for PID module (rabbit messaging queue) ...
+expected_message_connection_failed_only1 = '''Checking config for PID module (rabbit messaging queue) ...
  .. checking authentication and connection ...
  .. checking connection (this.is.my.only.host)... FAILED.
-Config for PID module (rabbit messaging queue) .. FAILED!
-********************************************************************
-*** PROBLEM IN SETTING UP                                        ***
-*** RABBIT MESSAGING QUEUE (PID MODULE)                          ***
-*** CONNECTION TO THE PID MESSAGING QUEUE FAILED DEFINITIVELY:   ***
-***  - host "this.is.my.only.host": Connection failure.          ***
-*** PLEASE NOTIFY handle@dkrz.de AND INCLUDE THIS ERROR MESSAGE. ***
-********************************************************************'''
+ .. giving this node a lower priority..
+Config for PID module (rabbit messaging queue) .. FAILED!'''
 
-expected_message_connection_failed_several = '''Checking config for PID module (rabbit messaging queue) ...
+expected_message_connection_failed_only2 = '''*********************************************************************************
+*** PROBLEM IN SETTING UP                                                     ***
+*** RABBIT MESSAGING QUEUE (PID MODULE)                                       ***
+*** CONNECTION TO THE PID MESSAGING QUEUE FAILED DEFINITIVELY:                ***
+***  - host "this.is.my.only.host": Connection failure (wrong host or port?). ***
+*** PLEASE NOTIFY handle@dkrz.de AND INCLUDE THIS ERROR MESSAGE.              ***
+*********************************************************************************'''
+
+expected_message_connection_failed_several1 = '''Checking config for PID module (rabbit messaging queue) ...
  .. checking authentication and connection ...
  .. checking connection (this.is.my.favourite.host)... FAILED.
+ .. giving this node a lower priority..
  .. checking connection (mystery-tour.uk)... FAILED.
+ .. giving this node a lower priority..
  .. checking connection (tomato.salad-with-spam.fr)... FAILED.
-Config for PID module (rabbit messaging queue) .. FAILED!
-********************************************************************
-*** PROBLEM IN SETTING UP                                        ***
-*** RABBIT MESSAGING QUEUE (PID MODULE)                          ***
-*** CONNECTION TO THE PID MESSAGING QUEUE FAILED DEFINITIVELY:   ***
-***  - host "this.is.my.favourite.host": Connection failure.     ***
-***  - host "mystery-tour.uk": Connection failure.               ***
-***  - host "tomato.salad-with-spam.fr": Connection failure.     ***
-*** PLEASE NOTIFY handle@dkrz.de AND INCLUDE THIS ERROR MESSAGE. ***
-********************************************************************'''
+ .. giving this node a lower priority..
+Config for PID module (rabbit messaging queue) .. FAILED!'''
 
-expected_message_authentication_failed_only = '''Checking config for PID module (rabbit messaging queue) ...
+expected_message_connection_failed_several2 = '''**************************************************************************************
+*** PROBLEM IN SETTING UP                                                          ***
+*** RABBIT MESSAGING QUEUE (PID MODULE)                                            ***
+*** CONNECTION TO THE PID MESSAGING QUEUE FAILED DEFINITIVELY:                     ***
+***  - host "this.is.my.favourite.host": Connection failure (wrong host or port?). ***
+***  - host "mystery-tour.uk": Connection failure (wrong host or port?).           ***
+***  - host "tomato.salad-with-spam.fr": Connection failure (wrong host or port?). ***
+*** PLEASE NOTIFY handle@dkrz.de AND INCLUDE THIS ERROR MESSAGE.                   ***
+**************************************************************************************'''
+
+expected_message_authentication_failed_only1 = '''Checking config for PID module (rabbit messaging queue) ...
  .. checking authentication and connection ...
  .. checking authentication (this.is.my.only.host)... FAILED.
-Config for PID module (rabbit messaging queue) .. FAILED!
-*************************************************************************************************
+ .. giving this node a lower priority..
+Config for PID module (rabbit messaging queue) .. FAILED!'''
+
+expected_message_authentication_failed_only2 = '''*************************************************************************************************
 *** PROBLEM IN SETTING UP                                                                     ***
 *** RABBIT MESSAGING QUEUE (PID MODULE)                                                       ***
 *** CONNECTION TO THE PID MESSAGING QUEUE FAILED DEFINITIVELY:                                ***
@@ -80,13 +94,17 @@ Config for PID module (rabbit messaging queue) .. FAILED!
 *** PLEASE NOTIFY handle@dkrz.de AND INCLUDE THIS ERROR MESSAGE.                              ***
 *************************************************************************************************'''
 
-expected_message_authentication_failed_several = '''Checking config for PID module (rabbit messaging queue) ...
+expected_message_authentication_failed_several1 = '''Checking config for PID module (rabbit messaging queue) ...
  .. checking authentication and connection ...
  .. checking authentication (this.is.my.favourite.host)... FAILED.
+ .. giving this node a lower priority..
  .. checking authentication (mystery-tour.uk)... FAILED.
+ .. giving this node a lower priority..
  .. checking authentication (tomato.salad-with-spam.fr)... FAILED.
-Config for PID module (rabbit messaging queue) .. FAILED!
-******************************************************************************************************
+ .. giving this node a lower priority..
+Config for PID module (rabbit messaging queue) .. FAILED!'''
+
+expected_message_authentication_failed_several2 = '''******************************************************************************************************
 *** PROBLEM IN SETTING UP                                                                          ***
 *** RABBIT MESSAGING QUEUE (PID MODULE)                                                            ***
 *** CONNECTION TO THE PID MESSAGING QUEUE FAILED DEFINITIVELY:                                     ***
@@ -97,14 +115,16 @@ Config for PID module (rabbit messaging queue) .. FAILED!
 ******************************************************************************************************'''
 
 
-expected_message_channel_closed_only = '''Checking config for PID module (rabbit messaging queue) ...
+expected_message_channel_closed_only1 = '''Checking config for PID module (rabbit messaging queue) ...
  .. checking authentication and connection ...
  .. checking authentication and connection (this.is.my.only.host)... ok.
  .. checking authentication and connection ... ok.
  .. checking channel ...
  .. checking channel ... FAILED.
-Config for PID module (rabbit messaging queue) .. FAILED!
-********************************************************************
+ .. giving this node a lower priority..
+Config for PID module (rabbit messaging queue) .. FAILED!'''
+
+expected_message_channel_closed_only2 = '''********************************************************************
 *** PROBLEM IN SETTING UP                                        ***
 *** RABBIT MESSAGING QUEUE (PID MODULE)                          ***
 *** CONNECTION TO THE PID MESSAGING QUEUE FAILED DEFINITIVELY:   ***
@@ -113,17 +133,19 @@ Config for PID module (rabbit messaging queue) .. FAILED!
 ********************************************************************'''
 
 
-expected_message_first_channel_closed_then_ok = '''Checking config for PID module (rabbit messaging queue) ...
+expected_message_first_channel_closed_then_ok1 = '''Checking config for PID module (rabbit messaging queue) ...
  .. checking authentication and connection ...
  .. checking authentication and connection (this.is.my.favourite.host)... ok.
  .. checking authentication and connection ... ok.
  .. checking channel ...
  .. checking channel ... FAILED.
+ .. giving this node a lower priority..
  .. checking authentication and connection ...
  .. checking authentication and connection (mystery-tour.uk)... ok.
  .. checking authentication and connection ... ok.
  .. checking channel ...
  .. checking channel ... FAILED.
+ .. giving this node a lower priority..
  .. checking authentication and connection ...
  .. checking authentication and connection (tomato.salad-with-spam.fr)... ok.
  .. checking authentication and connection ... ok.
@@ -133,3 +155,5 @@ expected_message_first_channel_closed_then_ok = '''Checking config for PID modul
  .. checking exchange ... ok.
 Config for PID module (rabbit messaging queue).. ok.
 Successful connection to PID messaging queue at "tomato.salad-with-spam.fr".'''
+
+expected_message_first_channel_closed_then_ok2 = None
--- tests/testcases/check_tests.py
+++ tests/testcases/check_tests.py
@@ -92,19 +92,22 @@ class CheckTestCase(unittest.TestCase):
 
         # Run code to be tested and capture stout:
         with tests.utils.captureconsoleoutput.captured_output() as (out, err):
-            esgfpid.check.check_pid_queue_availability(
+            returned_msg = esgfpid.check.check_pid_queue_availability(
                 connector = testconnector,
                 print_to_console = True,
                 print_success_to_console = True
             )
 
         # Expected message:
-        expected_message = tests.resources.error_messages.expected_message_ok
+        expected_message = tests.resources.error_messages.expected_message_ok1
+        expected_return = tests.resources.error_messages.expected_message_ok2
 
         # Check result:
         output = out.getvalue().strip()
         self.assertEquals(expected_message, output,
-            'Wrong error message.\n\nWe expected:\n\n'+expected_message+'\n\nWe got:\n\n'+output+'\n')
+            'Wrong stdout message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_message, output))
+        self.assertEquals(expected_return, returned_msg,
+            'Wrong return message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_return, returned_msg))
 
     #
     # Connection failures
@@ -128,19 +131,22 @@ class CheckTestCase(unittest.TestCase):
 
         # Run code to be tested and capture stout:
         with tests.utils.captureconsoleoutput.captured_output() as (out, err):
-            esgfpid.check.check_pid_queue_availability(
+            returned_msg = esgfpid.check.check_pid_queue_availability(
                 connector = testconnector,
                 print_to_console = True,
                 print_success_to_console = True
             )
 
         # Expected message:
-        expected_message = tests.resources.error_messages.expected_message_connection_failed_only
+        expected_message = tests.resources.error_messages.expected_message_connection_failed_only1
+        expected_return = tests.resources.error_messages.expected_message_connection_failed_only2
 
         # Check result:
         output = out.getvalue().strip()
         self.assertEquals(expected_message, output,
-            'Wrong error message.\n\nWe expected:\n\n'+expected_message+'\n\nWe got:\n\n'+output+'\n')
+            'Wrong stdout message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_message, output))
+        self.assertEquals(expected_return, returned_msg,
+            'Wrong return message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_return, returned_msg))
 
     @mock.patch('esgfpid.check.RabbitChecker._RabbitChecker__pika_blocking_connection')
     def test_run_check_connection_failed_unknown(self, connection_patch):
@@ -161,19 +167,24 @@ class CheckTestCase(unittest.TestCase):
 
         # Run code to be tested and capture stout:
         with tests.utils.captureconsoleoutput.captured_output() as (out, err):
-            esgfpid.check.check_pid_queue_availability(
+            returned_msg = esgfpid.check.check_pid_queue_availability(
                 connector = testconnector,
                 print_to_console = True,
                 print_success_to_console = True
             )
 
+
         # Expected message:
-        expected_message = tests.resources.error_messages.expected_message_connection_failed_unknown
+        expected_message = tests.resources.error_messages.expected_message_connection_failed_unknown1
+        expected_return = tests.resources.error_messages.expected_message_connection_failed_unknown2
+
         
         # Check result:
         output = out.getvalue().strip()
         self.assertEquals(expected_message, output,
-            'Wrong error message.\n\nWe expected:\n\n'+expected_message+'\n\nWe got:\n\n'+output+'\n')
+            'Wrong stdout message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_message, output))
+        self.assertEquals(expected_return, returned_msg,
+            'Wrong return message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_return, returned_msg))
 
 
     '''Test if the correct error message gets printed by the check method on connection failure.'''
@@ -206,19 +217,22 @@ class CheckTestCase(unittest.TestCase):
 
         # Run code to be tested and capture stout:
         with tests.utils.captureconsoleoutput.captured_output() as (out, err):
-            esgfpid.check.check_pid_queue_availability(
+            returned_msg = esgfpid.check.check_pid_queue_availability(
                 connector = testconnector,
                 print_to_console = True,
                 print_success_to_console = True
             )
 
         # Expected message:
-        expected_message = tests.resources.error_messages.expected_message_connection_failed_several
+        expected_message = tests.resources.error_messages.expected_message_connection_failed_several1
+        expected_return = tests.resources.error_messages.expected_message_connection_failed_several2
 
         # Check result:
         output = out.getvalue().strip()
         self.assertEquals(expected_message, output,
-            'Wrong error message.\n\nWe expected:\n\n'+expected_message+'\n\nWe got:\n\n'+output+'\n')
+            'Wrong return message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_message, output))
+        self.assertEquals(expected_return, returned_msg,
+            'Wrong stdout message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_return, returned_msg))
 
     @mock.patch('esgfpid.check.RabbitChecker._RabbitChecker__pika_blocking_connection')
     def test_run_check_first_connection_failed(self, connection_patch):
@@ -252,27 +266,29 @@ class CheckTestCase(unittest.TestCase):
         # Define the replacement for the patched method:
         def different_mock_response_depending_on_host(params):
             if params.host == 'tomato.salad-with-spam.fr':
-                return tests.mocks.pikamock.MockPikaBlockingConnection(mock.MagicMock())
+                return tests.resources.pikamock.MockPikaBlockingConnection(mock.MagicMock())
             else:
                 raise pika.exceptions.ConnectionClosed      
         connection_patch.side_effect = different_mock_response_depending_on_host
 
         # Run code to be tested and capture stout:
         with tests.utils.captureconsoleoutput.captured_output() as (out, err):
-            esgfpid.check.check_pid_queue_availability(
+            returned_msg = esgfpid.check.check_pid_queue_availability(
                 connector = testconnector,
                 print_to_console = True,
                 print_success_to_console = True
             )
 
         # Expected message:
-        expected_message = tests.resources.error_messages.expected_message_first_connection_failed_then_ok
+        expected_message = tests.resources.error_messages.expected_message_first_connection_failed_then_ok1
+        expected_return = tests.resources.error_messages.expected_message_first_connection_failed_then_ok2
 
         # Check result:
         output = out.getvalue().strip()
         self.assertEquals(expected_message, output,
-            'Wrong error message.\n\nWe expected:\n\n'+expected_message+'\n\nWe got:\n\n'+output+'\n')
-
+            'Wrong stdout message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_message, output))
+        self.assertEquals(expected_return, returned_msg,
+            'Wrong return message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_return, returned_msg))
 
 
     #
@@ -346,19 +362,22 @@ class CheckTestCase(unittest.TestCase):
 
         # Run code to be tested and capture stout:
         with tests.utils.captureconsoleoutput.captured_output() as (out, err):
-            esgfpid.check.check_pid_queue_availability(
+            returned_msg = esgfpid.check.check_pid_queue_availability(
                 connector = testconnector,
                 print_to_console = True,
                 print_success_to_console = True
             )
 
         # Expected message:
-        expected_message = tests.resources.error_messages.expected_message_authentication_failed_several
+        expected_message = tests.resources.error_messages.expected_message_authentication_failed_several1
+        expected_return = tests.resources.error_messages.expected_message_authentication_failed_several2
 
         # Check result:
         output = out.getvalue().strip()
         self.assertEquals(expected_message, output,
-            'Wrong error message.\n\nWe expected:\n\n'+expected_message+'\n\nWe got:\n\n'+output+'\n')
+            'Wrong stdout message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_message, output))
+        self.assertEquals(expected_return, returned_msg,
+            'Wrong return message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_return, returned_msg))
 
     #
     # Channel error
@@ -381,19 +400,22 @@ class CheckTestCase(unittest.TestCase):
 
         # Run code to be tested and capture stout:
         with tests.utils.captureconsoleoutput.captured_output() as (out, err):
-            esgfpid.check.check_pid_queue_availability(
+            returned_msg = esgfpid.check.check_pid_queue_availability(
                 connector = testconnector,
                 print_to_console = True,
                 print_success_to_console = True
             )
 
         # Expected message:
-        expected_message = tests.resources.error_messages.expected_message_channel_closed_only
+        expected_message = tests.resources.error_messages.expected_message_channel_closed_only1
+        expected_return = tests.resources.error_messages.expected_message_channel_closed_only2
 
         # Check result:
         output = out.getvalue().strip()
         self.assertEquals(expected_message, output,
-            'Wrong error message.\n\nWe expected:\n\n'+expected_message+'\n\nWe got:\n\n'+output+'\n')
+            'Wrong stdout message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_message, output))
+        self.assertEquals(expected_return, returned_msg,
+            'Wrong return message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_return, returned_msg))
 
     @mock.patch('esgfpid.check.RabbitChecker._RabbitChecker__pika_blocking_connection')
     def test_run_check_channel_error_first(self, connection_patch):
@@ -436,16 +458,19 @@ class CheckTestCase(unittest.TestCase):
 
         # Run code to be tested and capture stout:
         with tests.utils.captureconsoleoutput.captured_output() as (out, err):
-            esgfpid.check.check_pid_queue_availability(
+            returned_msg = esgfpid.check.check_pid_queue_availability(
                 connector = testconnector,
                 print_to_console = True,
                 print_success_to_console = True
             )
 
         # Expected message:
-        expected_message = tests.resources.error_messages.expected_message_first_channel_closed_then_ok
+        expected_message = tests.resources.error_messages.expected_message_first_channel_closed_then_ok1
+        expected_return = tests.resources.error_messages.expected_message_first_channel_closed_then_ok2
 
         # Check result:
         output = out.getvalue().strip()
         self.assertEquals(expected_message, output,
-            'Wrong error message.\n\nWe expected:\n\n'+expected_message+'\n\nWe got:\n\n'+output+'\n')
+            'Wrong stdout message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_message, output))
+        self.assertEquals(expected_return, returned_msg,
+            'Wrong return message.\n\nWe expected:\n\n%s\n\nWe got:\n\n%s\n' % (expected_return, returned_msg))
