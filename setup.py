6f5dd64e42cf54074dc2610b0d32c38634389d79
Merret Buurman <merretbuurman@users.noreply.github.com>
2021-03-17 16:04:46 +0100

    Increment version number to 0.8.2-dev

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ except(IOError, ImportError):
 
 setup(
     name='esgfpid',
-    version='0.8.1',
+    version='0.8.2-dev',
     author='Merret Buurman, German Climate Computing Centre (DKRZ)',
     author_email='buurman@dkrz.de',
     url='https://github.com/IS-ENES-Data/esgf-pid',
