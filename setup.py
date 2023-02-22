cf7395b5d34b99b098bf13d5184fe8ded57f6caf
Merret Buurman <buurman@dkrz.de>
2023-02-22 18:23:21 +0100

    Increment version number to 0.8.3-dev

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ except(IOError, ImportError):
 
 setup(
     name='esgfpid',
-    version='0.8.2',
+    version='0.8.3-dev',
     author='Merret Buurman, German Climate Computing Centre (DKRZ)',
     author_email='buurman@dkrz.de',
     url='https://github.com/IS-ENES-Data/esgf-pid',
